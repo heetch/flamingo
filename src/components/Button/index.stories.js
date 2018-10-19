fb9247dadbe57a313ab6d1171ced28e5f74b7836
ugogo <hello@onali-ugo.fr>
2018-10-19 15:30:45 +0200

    Add Button as link case

--- src/components/Button/index.stories.js
+++ src/components/Button/index.stories.js
@@ -7,18 +7,24 @@ import Button from "./";
 const intents = Object.values(INTENTS);
 const states = ["default", "hover", "active", "disabled"];
 
-storiesOf("Button", module).add("With intents", () =>
-  intents.map(intent =>
-    states.map(state => (
-      <div>
-        <Button
-          key={`${intent}-${state}`}
-          className={`is-${state}`}
-          intent={intent}
-        >
-          {intent} :{state}
-        </Button>
-      </div>
-    ))
+storiesOf("Button", module)
+  .add("With intents", () =>
+    intents.map(intent =>
+      states.map(state => (
+        <div>
+          <Button
+            key={`${intent}-${state}`}
+            className={`is-${state}`}
+            intent={intent}
+          >
+            {intent} :{state}
+          </Button>
+        </div>
+      ))
+    )
   )
-);
+  .add("As link", () => (
+    <a href="heetch.com" onClick={e => e.preventDefault()}>
+      <Button>Link</Button>
+    </a>
+  ));
