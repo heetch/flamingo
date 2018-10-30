b9dd56de29cdc7d16e0a12a636100ef9d7257e5b
ugogo <hello@onali-ugo.fr>
2018-10-30 18:12:52 +0100

    Use squared icons

--- src/constants/icons.js
+++ src/constants/icons.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 export const IconArrowDown = (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 16" version="1">
-    <path d="M3 1L0 3l12 12L24 3l-3-2-9 9z" />
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" version="1">
+    <path d="M3 5L0 7l12 12L24 7l-3-2-9 9z" />
   </svg>
 );
 
 export const IconArrowUp = (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 16" version="1">
-    <path d="M3 15l-3-2L12 1l12 12-3 2-9-9z" />
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" version="1">
+    <path d="M3 19l-3-2L12 5l12 12-3 2-9-9z" />
   </svg>
 );
