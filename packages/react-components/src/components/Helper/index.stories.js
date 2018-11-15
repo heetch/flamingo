3c26eb11ae1f34211fe44109218e7074fcc655f2
ugogo <hello@onali-ugo.fr>
2018-11-15 12:11:55 +0100

    Remove last version

--- packages/react-components/src/components/Helper/index.stories.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-
-import { Helper } from "../";
-
-storiesOf("Helper", module).add("Default", () => <Helper>Input helper</Helper>);
