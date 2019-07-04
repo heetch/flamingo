8fa3af628e4424db5d866ae15b83477aeafaee85
Przemek Krupiński <przemek@heetch.com>
2019-07-04 13:33:07 +0200

    Upgraded Storybook and implemented some storybook addons

--- packages/react/.storybook/config.js
+++ packages/react/.storybook/config.js
@@ -1,5 +1,6 @@
-import { configure } from "@storybook/react";
-
+import { addParameters, configure, addDecorator } from "@storybook/react";
+import centered from "@storybook/addon-centered/react";
+import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
 import "normalize.css";
 import "../../css/dist/index.css";
 
@@ -8,5 +9,48 @@ const req = require.context("../src/components", true, /\.stories\.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
+
+addDecorator(centered);
+
+addParameters({
+  options: {
+    name: "Heetch Flamingo",
+    addonPanelInRight: true,
+    hierarchySeparator: /\//,
+  },
+});
+
+const flamingoViewports = {
+  BreakpointS: {
+    name: "Flamingo Breakpoint S",
+    styles: {
+      width: "460px",
+      height: "963px",
+    },
+  },
+  BreakpointM: {
+    name: "Flamingo Breakpoint M",
+    styles: {
+      width: "800px",
+      height: "963px",
+    },
+  },
+  BreakpointL: {
+    name: "Flamingo Breakpoint L",
+    styles: {
+      width: "1100px",
+      height: "963px",
+    },
+  },
+};
+
+addParameters({
+  viewport: {
+    viewports: {
+      ...flamingoViewports,
+      ...INITIAL_VIEWPORTS,
+    },
+  },
+});
 
 configure(loadStories, module);
