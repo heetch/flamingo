d8d51a789504d55d2ef3295ef413182b48894985
ugogo <hello@onali-ugo.fr>
2019-07-05 10:55:27 +0200

    Update stories

--- packages/react/src/components/Button/index.stories.js
+++ packages/react/src/components/Button/index.stories.js
@@ -14,21 +14,28 @@ const Link = ({ children, to, ...props }) => (
 );
 
 storiesOf("Button", module)
-  .add("With intents", () =>
-    intents.map(intent =>
-      states.map(state => (
-        <div key={`${intent}-${state}`}>
-          <Button
-            className={`is-${state}`}
-            disabled={state === "disabled"}
-            intent={intent}
-          >
-            {intent} :{state}
-          </Button>
+  .add("With intents", () => (
+    <div style={{ display: "flex" }}>
+      {intents.map(intent => (
+        <div
+          key={intent}
+          style={{ textAlign: "center", padding: "0 var(--space-s)" }}
+        >
+          {states.map(state => (
+            <div key={`${intent}-${state}`}>
+              <Button
+                className={`is-${state}`}
+                disabled={state === "disabled"}
+                intent={intent}
+              >
+                {intent} ({state})
+              </Button>
+            </div>
+          ))}
         </div>
-      ))
-    )
-  )
+      ))}
+    </div>
+  ))
   .add("As link", () => (
     <Button as={Link} to="https://www.heetch.com" target="_blank">
       Link to heetch.com
