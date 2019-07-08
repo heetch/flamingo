c1f68e68a4732c22133e71fc221479d6947b2042
ugogo <hello@onali-ugo.fr>
2019-07-08 16:09:26 +0200

    Add IconButton component

---
+++ packages/react/src/components/IconButton/index.stories.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { boolean, select, withKnobs } from "@storybook/addon-knobs";
+
+import { ICONS } from "../../constants/";
+import IconButton from "./";
+
+const states = ["default", "hover", "active", "disabled"];
+const icon = "IconAdd";
+
+const stories = storiesOf("IconButton", module);
+
+stories.addDecorator(withKnobs);
+
+stories.add("With states", () =>
+  states.map(state => (
+    <div key={state} style={{ padding: "var(--space-s)" }}>
+      <IconButton
+        className={`is-${state}`}
+        disabled={state === "disabled"}
+        icon={icon}
+      />
+    </div>
+  ))
+);
+
+stories.add("Playground", () => (
+  <IconButton
+    disabled={boolean("Disabled", false)}
+    icon={select("Icon", ICONS, icon)}
+  />
+));
--- packages/react/src/components/index.js
+++ packages/react/src/components/index.js
@@ -2,6 +2,7 @@ export { default as Button } from "./Button";
 export { default as Heading } from "./Heading";
 export { default as Helper } from "./Helper";
 export { default as Icon } from "./Icon";
+export { default as IconButton } from "./IconButton";
 export { default as Input } from "./Input";
 export { default as Label } from "./Label";
 export { default as Select } from "./Select";
