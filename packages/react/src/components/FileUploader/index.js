a662b5ea42ca8affb18d46ce5ace60d3a44f2c63
ugogo <hello@onali-ugo.fr>
2019-07-12 10:20:24 +0200

    Prepare states

--- packages/react/src/components/FileUploader/index.js
+++ packages/react/src/components/FileUploader/index.js
@@ -1,5 +1,104 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const FileUploader = () => <input type="file" />;
+import Icon from "../Icon";
+import Text from "../Text";
+
+import { ICONS } from "../../constants";
+
+const defaultTranslate = ({ defaultText }) => defaultText;
+
+const texts = {
+  add_document: {
+    id: "flamingo-file-uploader-add-document",
+    defaultText: "Add a document",
+  },
+  uploading: {
+    id: "flamingo-file-uploader-uploading",
+    defaultText: "Uploading...",
+  },
+  error: {
+    id: "flamingo-file-uploader-error-title",
+    defaultText: "An error just happened",
+  },
+  try_again: {
+    id: "flamingo-file-uploader-error-try-again",
+    defaultText: "Please try again",
+  },
+};
+
+const STATES = {
+  DEFAULT: "default",
+  ERRORED: "errored",
+  LOADING: "loading",
+  SUCCESS: "success",
+};
+
+const FileUploader = ({
+  children,
+  name,
+  onChange,
+  state,
+  translate,
+  ...props
+}) => (
+  <div className="FileUploader" {...props}>
+    {state === STATES.DEFAULT && (
+      <label
+        className="FileUploader-state FileUploader-state--empty"
+        htmlFor={name}
+      >
+        <Icon icon={ICONS.IconAdd} />
+        <Text>{translate(texts.add_document)}</Text>
+      </label>
+    )}
+
+    {state === STATES.LOADING && (
+      <div className="FileUploader-state FileUploader-state--uploading">
+        <Text>{translate(texts.uploading)}</Text>
+      </div>
+    )}
+
+    {state === STATES.ERRORED && (
+      <div className="FileUploader-state FileUploader-state--error">
+        <Text className="FileUploader-errorState-title">
+          {translate(texts.error)}
+        </Text>
+
+        <Text className="FileUploader-errorState-tryAgain">
+          {translate(texts.try_again)}
+        </Text>
+      </div>
+    )}
+
+    {state === STATES.SUCCESS && (
+      <div className="FileUploader-state FileUploader-state--success">
+        <Text>Success state, wait for Juanito for more details</Text>
+      </div>
+    )}
+
+    {children}
+
+    <div className="FileUploader-inputContainer">
+      <input type="file" id={name} name={name} onChange={onChange} />
+    </div>
+  </div>
+);
+
+FileUploader.propTypes = {
+  children: PropTypes.node,
+  name: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  state: PropTypes.oneOf(Object.values(STATES)),
+  translate: PropTypes.func,
+};
+
+FileUploader.defaultProps = {
+  children: undefined,
+  state: STATES.DEFAULT,
+  translate: defaultTranslate,
+};
+
+FileUploader.STATES = STATES;
 
 export default FileUploader;
--- packages/react/src/components/FileUploader/index.stories.js
+++ packages/react/src/components/FileUploader/index.stories.js
@@ -1,11 +1,32 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import { withKnobs } from "@storybook/addon-knobs";
+import { select, withKnobs } from "@storybook/addon-knobs";
 
 import FileUploader from ".";
 
-const stories = storiesOf("FileUploader", module);
+const { STATES } = FileUploader;
+
+const inputName = "input-name";
+const onChange = console.log;
+
+const stories = storiesOf("Uploaders/FileUploader", module);
 
 stories.addDecorator(withKnobs);
 
-stories.add("Playground", () => <FileUploader />);
+stories.add("With states", () =>
+  Object.values(STATES).map(state => (
+    <FileUploader
+      onChange={onChange}
+      state={state}
+      name={`inputName-${state}`}
+    />
+  ))
+);
+
+stories.add("Playground", () => (
+  <FileUploader
+    name={inputName}
+    onChange={onChange}
+    state={select("state", STATES)}
+  />
+));
