19271ff55d179593930a6c40ff3bc1605c18c860
David Guyon <davidg@heetch.com>
2019-07-30 10:52:02 +0200

    Introduce Zeit (now) auto deploy

---
+++ now-build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#
+# Zeit (now for github)
+
+set -euo pipefail
+
+umask 077
+
+IFS=$'\n\t'
+
+# prerequisites
+yarn --cwd  packages/css install
+yarn --cwd packages/react install
+
+# build
+yarn --cwd packages/react run build:storybook
