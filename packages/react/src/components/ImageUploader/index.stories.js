2eb71de6d1ea1466e30ae6d090d1ab99e0b08e44
ugogo <hello@onali-ugo.fr>
2019-07-31 17:46:21 +0200

    Add within form story for ImageUploader

--- packages/react/src/components/ImageUploader/index.stories.js
+++ packages/react/src/components/ImageUploader/index.stories.js
@@ -4,6 +4,8 @@ import { boolean, withKnobs } from "@storybook/addon-knobs";
 import { action } from "@storybook/addon-actions";
 
 import ImageUploader from ".";
+import Helper from "../Helper";
+import Label from "../Label";
 
 const inputName = "input-name";
 const stories = storiesOf("Uploaders/ImageUploader", module);
@@ -16,4 +18,16 @@ stories.add("Playground", () => (
     onChange={action("onChange")}
     multiple={boolean("Has multiple files", true)}
   />
+));
+
+stories.add("Within form", () => (
+  <>
+    <Label htmlFor={inputName}>Image uploader label</Label>
+    <ImageUploader
+      name={inputName}
+      onChange={action("onChange")}
+      multiple={boolean("Has multiple files", true)}
+    />
+    <Helper>Helper text</Helper>
+  </>
 ));
