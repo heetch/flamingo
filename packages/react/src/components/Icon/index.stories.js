fd7d4b320db6f57d9f3685b25fd3c2d7dd133c94
ugogo <hello@onali-ugo.fr>
2019-07-31 15:23:23 +0200

    Display the icon value alongside the svg

--- packages/react/src/components/Icon/index.stories.js
+++ packages/react/src/components/Icon/index.stories.js
@@ -2,21 +2,29 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { select, withKnobs } from "@storybook/addon-knobs";
 
-import { ICONS, ICON_SIZES } from "../../constants";
+import Helper from "../Helper";
 import Icon from ".";
+
+import { ICONS, ICON_SIZES } from "../../constants";
 
 const icons = Object.keys(ICONS);
 const sizes = Object.values(ICON_SIZES);
 
-const stories = storiesOf("Icon", module);
+const stories = storiesOf("Icons", module);
 
 stories.addDecorator(withKnobs);
 
 stories.add("All", () => (
-  <div style={{ padding: "var(--space-xl)" }}>
+  <div
+    style={{ display: "flex", flexWrap: "wrap", padding: "var(--space-xl)" }}
+  >
     {icons.map(icon => (
-      <div style={{ padding: "var(--space-s)", display: "inline-block" }}>
+      <div
+        key={icon}
+        style={{ padding: "var(--space-s)", width: 150, textAlign: "center" }}
+      >
         <Icon key={`Icon--${icon}`} icon={icon} />
+        <Helper>{icon}</Helper>
       </div>
     ))}
   </div>
