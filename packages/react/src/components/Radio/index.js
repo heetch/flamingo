30abedb28601b334c845e7fa5f341b44e9588252
Przemek Krupiński <przemek@heetch.com>
2019-07-31 16:11:08 +0200

    Implemented Radio component

---
+++ packages/react/src/components/Radio/index.js
@@ -0,0 +1,53 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Radio = ({
+  disabled,
+  children,
+  checked,
+  helper,
+  name,
+  value,
+  onChange,
+}) => {
+  const buildInputTag = () => (
+    <input
+      defaultChecked={checked}
+      disabled={disabled}
+      className="Radio"
+      type="radio"
+      name={name}
+      value={value}
+      onChange={onChange}
+    />
+  );
+
+  return children ? (
+    <label className="Radio-label">
+      {buildInputTag()}
+      <div>
+        {children}
+        {helper && <p className="Radio-helper">{helper}</p>}
+      </div>
+    </label>
+  ) : (
+    buildInputTag()
+  );
+};
+
+Radio.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  checked: PropTypes.bool,
+  disabled: PropTypes.bool,
+  children: PropTypes.string,
+  helper: PropTypes.string,
+};
+
+Radio.defaultProps = {
+  checked: false,
+  disabled: false,
+  children: undefined,
+  helper: undefined,
+};
+
+export default Radio;
