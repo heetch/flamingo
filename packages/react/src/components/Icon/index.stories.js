d19408df0bc24b6a1f036dfc3340f04c88e76ba5
ugogo <hello@onali-ugo.fr>
2019-07-31 15:48:52 +0200

    Add a search

--- packages/react/src/components/Icon/index.stories.js
+++ packages/react/src/components/Icon/index.stories.js
@@ -3,6 +3,8 @@ import { storiesOf } from "@storybook/react";
 import { select, withKnobs } from "@storybook/addon-knobs";
 
 import Helper from "../Helper";
+import Input from "../Input";
+import Label from "../Label";
 import Icon from ".";
 
 import { ICONS, ICON_SIZES } from "../../constants";
@@ -10,25 +12,50 @@ import { ICONS, ICON_SIZES } from "../../constants";
 const icons = Object.keys(ICONS);
 const sizes = Object.values(ICON_SIZES);
 
+const IconsFilterer = () => {
+  const [filter, setFilter] = React.useState("");
+
+  const displayedIcons = icons.filter(icon =>
+    icon.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  const handleInputChange = e => setFilter(e.target.value);
+
+  return (
+    <div>
+      <Label htmlFor="icon-search">Search</Label>
+      <Input id="icon-search" onChange={handleInputChange} />
+
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          padding: "var(--space-xl)",
+        }}
+      >
+        {displayedIcons.map(icon => (
+          <div
+            key={icon}
+            style={{
+              padding: "var(--space-s)",
+              width: 150,
+              textAlign: "center",
+            }}
+          >
+            <Icon key={`Icon--${icon}`} icon={icon} />
+            <Helper>{icon}</Helper>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
 const stories = storiesOf("Icons", module);
 
 stories.addDecorator(withKnobs);
 
-stories.add("All", () => (
-  <div
-    style={{ display: "flex", flexWrap: "wrap", padding: "var(--space-xl)" }}
-  >
-    {icons.map(icon => (
-      <div
-        key={icon}
-        style={{ padding: "var(--space-s)", width: 150, textAlign: "center" }}
-      >
-        <Icon key={`Icon--${icon}`} icon={icon} />
-        <Helper>{icon}</Helper>
-      </div>
-    ))}
-  </div>
-));
+stories.add("All", () => <IconsFilterer />);
 
 stories.add("Playground", () => (
   <Icon
