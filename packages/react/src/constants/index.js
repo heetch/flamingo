d68d014464f1a1534fc3deb11422ea26a4d08c91
ugogo <hello@onali-ugo.fr>
2019-10-23 19:54:05 +0200

    Uniformize UiText types

--- packages/react/src/constants/index.js
+++ packages/react/src/constants/index.js
@@ -15,19 +15,6 @@ export const INTENTS = {
   TERTIARY: 'tertiary',
 };
 
-export const UI_TYPES = {
-  h1: 'heading-1',
-  h2: 'heading-2',
-  h3: 'heading-3',
-  h4: 'heading-4',
-  h5: 'heading-5',
-  h6: 'heading-6',
-  content: 'content',
-  contentBold: 'contentBold',
-  subContent: 'subContent',
-  subContentBold: 'subContentBold',
-};
-
 export const refShapes = [
   PropTypes.func,
   PropTypes.shape({ current: PropTypes.instanceOf(Element) }),
