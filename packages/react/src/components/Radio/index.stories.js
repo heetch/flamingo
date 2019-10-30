894d89bb43a343915bd3789ec9b42433752bdd6b
ugogo <hello@onali-ugo.fr>
2019-10-30 11:51:45 +0100

    Fix Radio id prop in stories

--- packages/react/src/components/Radio/index.stories.js
+++ packages/react/src/components/Radio/index.stories.js
@@ -19,33 +19,33 @@ stories.add('All states', () => (
     <div style={{ display: 'flex' }}>
       <div style={{ marginRight: 'var(--space-xl)', width: '10rem' }}>
         <Heading level={2}>Default</Heading>
-        <Radio {...defaultProps} name='r1' />
+        <Radio {...defaultProps} id='r1-1' name='r1' />
       </div>
       <div>
         <Heading level={2}>& disabled</Heading>
-        <Radio {...defaultProps} name='r2' disabled />
+        <Radio {...defaultProps} id='r2-1' name='r2' disabled />
       </div>
     </div>
 
     <div style={{ display: 'flex' }}>
       <div style={{ marginRight: 'var(--space-xl)', width: '10rem' }}>
         <Heading level={2}>Checked</Heading>
-        <Radio {...defaultProps} name='r3' checked />
+        <Radio {...defaultProps} id='r3-1' name='r3' checked />
       </div>
       <div>
         <Heading level={2}>& disabled</Heading>
-        <Radio {...defaultProps} name='r4' checked disabled />
+        <Radio {...defaultProps} id='r4-1' name='r4' checked disabled />
       </div>
     </div>
 
     <div style={{ display: 'flex' }}>
       <div style={{ marginRight: 'var(--space-xl)', width: '10rem' }}>
         <Heading level={2}>No text</Heading>
-        <Radio onChange={defaultProps.onChange} name='r5' />
+        <Radio onChange={defaultProps.onChange} id='r5-1' name='r5' />
       </div>
       <div>
         <Heading level={2}>With helper</Heading>
-        <Radio {...defaultProps} name='r6' helper='With helper'>
+        <Radio {...defaultProps} id='r6-1' name='r6' helper='With helper'>
           Label
         </Radio>
       </div>
@@ -56,6 +56,7 @@ stories.add('All states', () => (
 stories.add('Playground', () => (
   <Radio
     {...defaultProps}
+    id='r7-1'
     name='r7'
     disabled={boolean('Is disabled?', false)}
     helper={text('Label helper', 'Label helper')}
