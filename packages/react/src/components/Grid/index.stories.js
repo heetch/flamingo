b1f1f8488fc848b0e729b4ad8f22397b04704aeb
ugogo <hello@onali-ugo.fr>
2019-11-14 12:10:44 +0100

    Add responsive options

--- packages/react/src/components/Grid/index.stories.js
+++ packages/react/src/components/Grid/index.stories.js
@@ -8,7 +8,7 @@ import Heading from '../Heading';
 import UiText from '../UiText';
 
 // eslint-disable-next-line react/prop-types
-const ColInner = ({ children }) => (
+const ColInner = ({ children, style }) => (
   <div
     style={{
       padding: 'var(--f-space--m)',
@@ -16,6 +16,7 @@ const ColInner = ({ children }) => (
       color: 'var(--f-color-text--white)',
       backgroundColor: 'var(--f-color-brandPrimary)',
       borderRadius: 'var(--f-borderRadius--m)',
+      ...style,
     }}
   >
     <UiText as='span' variant={UiText.VARIANTS.subContentBold}>
@@ -24,21 +25,66 @@ const ColInner = ({ children }) => (
   </div>
 );
 
+const gridProps = {
+  style: { marginBottom: 'var(--f-space--m)' },
+};
+
 const stories = storiesOf('Grid', module);
 
 stories.add('All sizes', () => (
   <>
     <Heading>Grid</Heading>
+    <Heading level={2}>Fluid columns</Heading>
 
-    <Grid>
-      <Grid.Col>
+    <Grid {...gridProps}>
+      <Grid.Col fluid>
         <ColInner>Fluid</ColInner>
       </Grid.Col>
-      <Grid.Col>
+      <Grid.Col fluid>
         <ColInner>Fluid</ColInner>
       </Grid.Col>
-      <Grid.Col>
+      <Grid.Col fluid>
         <ColInner>Fluid</ColInner>
+      </Grid.Col>
+    </Grid>
+
+    <Heading level={2}>Sized columns</Heading>
+
+    <Grid {...gridProps}>
+      <Grid.Col fluid>
+        <ColInner>Fluid</ColInner>
+      </Grid.Col>
+      <Grid.Col s={6}>
+        <ColInner>{`s={${6}}`}</ColInner>
+      </Grid.Col>
+      <Grid.Col fluid>
+        <ColInner>Fluid</ColInner>
+      </Grid.Col>
+    </Grid>
+
+    <Grid {...gridProps}>
+      <Grid.Col s={2}>
+        <ColInner>{`s={${2}}`}</ColInner>
+      </Grid.Col>
+      <Grid.Col s={10}>
+        <ColInner>{`s={${10}}`}</ColInner>
+      </Grid.Col>
+    </Grid>
+
+    <Heading level={2}>Responsive columns</Heading>
+
+    <Grid {...gridProps}>
+      <Grid.Col m={4} l={10}>
+        <ColInner>{`m={${4}} l={${10}}`}</ColInner>
+      </Grid.Col>
+      <Grid.Col m={4} l={2}>
+        <ColInner>{`m={${4}} l={${2}}`}</ColInner>
+      </Grid.Col>
+    </Grid>
+
+    <Grid {...gridProps}>
+      <Grid.Col m={2} l={2}>
+        <ColInner>{`m={${2}} l={${2}}`}</ColInner>
       </Grid.Col>
       <Grid.Col>
         <ColInner>Fluid</ColInner>
--- packages/react/src/components/GridCol/index.js
+++ packages/react/src/components/GridCol/index.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import cx from 'classnames';
 
-const GridCol = ({ children }) => <div className='f-Grid-col'>{children}</div>;
+const GridCol = ({ s, m, l, ...props }) => (
+  <div
+    className={cx('f-Grid-col', {
+      [`f-Grid-col--s${s}`]: s,
+      [`f-Grid-col--m${m}`]: m,
+      [`f-Grid-col--l${l}`]: l,
+    })}
+    {...props}
+  />
+);
 
 GridCol.propTypes = {
-  children: PropTypes.node,
+  s: PropTypes.number,
+  m: PropTypes.number,
+  l: PropTypes.number,
 };
 
 GridCol.defaultProps = {
-  children: undefined,
+  s: 0,
+  m: 0,
+  l: 0,
 };
 
 export default GridCol;
