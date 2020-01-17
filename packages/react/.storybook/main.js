b2efb55fb3f2859b36ed2a7d8630ca66980276f5
ugogo <hello@onali-ugo.fr>
2020-01-17 18:41:39 +0100

    Add missing preset

--- packages/react/.storybook/main.js
+++ packages/react/.storybook/main.js
@@ -1,6 +1,7 @@
 module.exports = {
   stories: ['../**/*.stories.js'],
   addons: [
+    '@storybook/preset-create-react-app',
     '@storybook/addon-knobs',
     '@storybook/addon-options',
     '@storybook/addon-actions',
