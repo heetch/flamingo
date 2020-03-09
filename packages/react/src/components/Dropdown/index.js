d61d3cc74764a66c5f125c51822812dff51d3855
ugogo <hello@onali-ugo.fr>
2020-03-09 17:08:55 +0100

    Create Dropdown component

---
+++ packages/react/src/components/Dropdown/index.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+import { Manager, Reference, Popper } from 'react-popper';
+
+const Dropdown = ({ children, options, placement }) => {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const open = () => setIsOpen(true);
+  const hide = () => setIsOpen(false);
+  const toggle = () => setIsOpen(!isOpen);
+
+  const [popperNode, setPopperNode] = React.useState();
+  const [refNode, setRefNode] = React.useState();
+
+  const renderFnProps = { isOpen, open, hide, toggle };
+
+  React.useEffect(() => {
+    const onDocumentClick = ({ target }) => {
+      if (!popperNode.contains(target) && !refNode.contains(target)) {
+        hide();
+      }
+    };
+
+    document.addEventListener('click', onDocumentClick);
+    return () => document.removeEventListener('click', onDocumentClick);
+  }, [popperNode, refNode]);
+
+  return (
+    <Manager>
+      <Reference innerRef={setRefNode}>
+        {({ ref }) => children({ ...renderFnProps, ref })}
+      </Reference>
+
+      <Popper
+        modifiers={{
+          offset: {
+            offset: '0 4px',
+          },
+        }}
+        innerRef={setPopperNode}
+        placement={placement}
+      >
+        {({ ref, style }) => (
+          <div
+            className={cx('f-DropdownList', { 'is-hidden': !isOpen })}
+            ref={ref}
+            style={style}
+            data-placement={placement}
+          >
+            {options(renderFnProps)}
+          </div>
+        )}
+      </Popper>
+    </Manager>
+  );
+};
+
+Dropdown.displayName = 'Dropdown';
+
+Dropdown.propTypes = {
+  children: PropTypes.func.isRequired,
+  options: PropTypes.func.isRequired,
+  placement: PropTypes.string, // https://popper.js.org/docs/v1/#Popper.placements
+};
+
+Dropdown.defaultProps = {
+  placement: 'bottom-end',
+};
+
+export default Dropdown;
---
+++ packages/react/src/components/Dropdown/index.stories.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+
+import Dropdown from '.';
+import Button from '../Button';
+import Heading from '../Heading';
+import Item from '../Item';
+
+const stories = storiesOf('Dropdown', module);
+
+stories.add('Playground', () => (
+  <>
+    <Heading>Dropdown</Heading>
+
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'baseline',
+        height: '11rem',
+        padding: '1rem',
+        border: '1px dashed var(--f-color-brandSecondary--light)',
+        borderRadius: 'var(--f-borderRadius--m)',
+      }}
+    >
+      <Dropdown
+        options={({ hide }) => (
+          <>
+            <Item
+              onClick={() => {
+                action('onItemClick');
+                hide();
+              }}
+            >
+              Batman
+            </Item>
+            <Item
+              onClick={() => {
+                action('onItemClick');
+                hide();
+              }}
+            >
+              Robin
+            </Item>
+          </>
+        )}
+      >
+        {({ ref, toggle }) => (
+          <Button ref={ref} onClick={toggle}>
+            Click me
+          </Button>
+        )}
+      </Dropdown>
+    </div>
+  </>
+));
