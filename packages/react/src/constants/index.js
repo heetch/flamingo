5750bfe765899d28451037cbf60e1310997ba19f
ugogo <hello@onali-ugo.fr>
2020-03-12 19:47:15 +0100

    Convert variables

--- packages/react/src/constants/index.js
+++ packages/react/src/constants/index.js
@@ -1,8 +1,84 @@
-import PropTypes from 'prop-types';
+export const BORDER_RADIUS = {
+  XL: '1.5rem', // 24px
+  L: '1rem', // 16px
+  M: '0.5rem', // 8px
+  S: '0.25rem', // 4px
+};
 
-export const refShapes = [
-  PropTypes.func,
-  PropTypes.shape({ current: PropTypes.instanceOf(Element) }),
-];
+export const BREAKPOINT = {
+  S: '28.75rem', // 460px
+  M: '50rem', // 800px
+  L: '68.75rem', // 1100px
+};
 
-export default { refShapes };
+export const COLOR = {
+  BRAND: {
+    PRIMARY: '#FF2773',
+    PRIMARY_LIGHT: '#FF4D95',
+    PRIMARY_INACTIVE: '#FC99BC',
+    SECONDARY: '#2B1E70',
+    SECONDARY_LIGHT: '#252D9B',
+  },
+  ICON: {
+    WHITE: '#fff',
+    DISABLED: '#dedede',
+    DARK: '#190134',
+  },
+  ELEMENT: {
+    PRIMARY: '#fff',
+    SECONDARY: '#685879',
+    TERTIARY: '#b3abbc',
+    INACTIVE: '#eaeaea',
+    ERROR: '#ee7d52',
+    SUCCESS: '#5cc689',
+    OVERLAY: '#2b1e70',
+  },
+  TEXT: {
+    PRIMARY: '#190134',
+    SECONDARY: '#685879',
+    TERTIARY: '#b3abbc',
+    LINK: '#252d9b',
+    WHITE: '#fff',
+  },
+};
+
+export const FONT_SIZE = {
+  XXS: '0.625rem', // 10px
+  XS: '0.75rem', // 12px
+  S: '0.875rem', // 14px
+  M: '1rem', // 16px
+  L: '1.25rem', // 20px
+  XL: '1.5rem', // 24px
+  XXL: '2rem', // 32px
+};
+
+export const FONT_WEIGHT = {
+  NORMAL: 400,
+  BOLD: 700,
+  BLACK: 800,
+};
+
+export const ICON_SIZE = {
+  S: '0.75rem', // 12px
+  M: '1rem', // 16px
+  L: '1.25rem', // 20px
+};
+
+export const LINE_HEIGHT = {
+  S: '1rem',
+  M: '1.4rem',
+  L: '1.5rem',
+  XL: '2rem',
+};
+
+export const SPACE = {
+  S: '0.25rem', // 4px
+  M: '0.5rem', // 8px
+  L: '0.75rem', // 12px
+  XL: '1rem', // 16px
+  XXL: '1.5rem', // 24px
+};
+
+export const Z_INDEX = {
+  OVERLAY: 100,
+};
