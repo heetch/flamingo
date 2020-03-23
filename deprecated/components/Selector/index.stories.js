722278a00c03d57c27ea0830714957e409c890c9
Obed Marquez Parlapiano <obed.marquezp@gmail.com>
2020-03-23 10:23:15 +0100

    V2/deprecated folders (#121)
    * Move css and reac comps into deprecated folder
    
    * Update readme file
    
    * Remove usage of `react` and `css` folders in other files
    
    * move react folder into  in the root

---
+++ deprecated/components/Selector/index.stories.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { object } from '@storybook/addon-knobs';
+
+import Heading from '../Heading';
+import Selector from './index';
+
+const defaultProps = () => ({
+  id: `selector-${Math.random()}`,
+  onChange: action('onChange'),
+});
+
+const stories = storiesOf('Form/Selector', module);
+
+stories.add('All states', () => (
+  <>
+    <Heading>Selector</Heading>
+    <Heading level={2}>States</Heading>
+
+    <Heading level={3}>Not selected</Heading>
+    <Selector
+      {...defaultProps()}
+      options={object('Not Selected options', [
+        { label: 'Luke', value: 1 },
+        { label: 'Han', value: 2 },
+        { label: 'Chewy', value: 3 },
+        { label: 'Leia', value: 4 },
+      ])}
+    />
+
+    <Heading level={3}>Hover</Heading>
+    <Selector
+      {...defaultProps()}
+      className='is-hover'
+      options={object('Hover', [
+        { label: 'Luke', value: 1 },
+        { label: 'Han', value: 2 },
+      ])}
+    />
+
+    <Heading level={3}>Disabled</Heading>
+    <Selector
+      {...defaultProps()}
+      options={object('Disabled options', [
+        { label: 'Luke', value: 1, disabled: true },
+        { label: 'Darth', value: 4 },
+        { label: 'Chewy', value: 3 },
+      ])}
+    />
+
+    <Heading level={3}>Selected</Heading>
+    <Selector
+      {...defaultProps()}
+      defaultIndex={1}
+      options={object('Selected options', [
+        { label: 'Luke', value: 1 },
+        { label: 'Han', value: 2 },
+      ])}
+    />
+
+    <Heading level={3}>Selected and disabled</Heading>
+    <Selector
+      {...defaultProps()}
+      defaultIndex={0}
+      disabled
+      options={object('Selected and disabled options', [
+        { label: 'Luke', value: 1 },
+        { label: 'Han', value: 2 },
+        { label: 'Chewy', value: 3 },
+      ])}
+    />
+
+    <Heading level={3}>Invalid</Heading>
+    <Selector
+      {...defaultProps()}
+      defaultIndex={1}
+      isInvalid
+      options={object('Invalid options', [
+        { label: 'Luke', value: 1 },
+        { label: 'Han', value: 2 },
+        { label: 'Chewy', value: 3 },
+      ])}
+    />
+
+    <Heading level={3}>Valid</Heading>
+    <Selector
+      {...defaultProps()}
+      defaultIndex={2}
+      isValid
+      options={object('Valid options', [
+        { label: 'Luke', value: 1 },
+        { label: 'Han', value: 2 },
+        { label: 'Chewy', value: 3 },
+      ])}
+    />
+  </>
+));
+
+stories.add('Playground', () => <Selector {...defaultProps()} />);
---
+++ deprecated/components/Spinner/index.stories.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { select } from '@storybook/addon-knobs';
+
+import Heading from '../Heading';
+import Icon from '../Icon';
+import Spinner from './index';
+
+const sizes = Object.values(Icon.SIZES);
+const stories = storiesOf('Spinner', module);
+
+stories.add('Playground', () => (
+  <>
+    <Heading>Spinner</Heading>
+    <Spinner size={select('Size', sizes, Icon.SIZES.M)} />
+  </>
+));
