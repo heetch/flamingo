eba5cfec5a66eb9a595f56d5d0958e87fd98d4d4
Obed <obed.imp@gmail.com>
2020-04-15 16:35:24 +0200

    Migrate IconButton

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import cx from 'classnames';
 import Spinner from '../Spinner';
-import Icon from '../Icon';
 
 const INTENTS = {
   PRIMARY: 'primary',
---
+++ src/components/IconButton/index.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+import styled from 'styled-components';
+
+import Button from '../Button';
+import Icon from '../Icon';
+
+const sizes = Object.values(Icon.SIZES);
+
+const IconButton = styled(
+  ({ className, icon, size, iconColor, disabled, ...props }) => (
+    <Button
+      className={cx('f-Button--icon', className)}
+      intent={Button.INTENTS.SECONDARY}
+      variant={Button.VARIANTS.MINIMAL}
+      size={size}
+      disabled={disabled}
+      {...props}
+    >
+      <Icon icon={icon} size={size} iconColor={!disabled && iconColor} />
+    </Button>
+  ),
+)`
+  padding: var(--f-space--m);
+  line-height: 1;
+  border-radius: 50%;
+  color: var(--f-color-icon--dark);
+
+  &:hover {
+    background-color: var(--f-color-element--inactive);
+    color: var(--f-color-icon--dark);
+  }
+
+  & + & {
+    margin-left: var(--f-space--m);
+  }
+
+  ${Icon} {
+    display: block;
+  }
+
+  @media (max-width: 460px) {
+    /* --f-breakpoint--s */
+    width: auto;
+    flex-grow: inherit;
+    padding: var(--f-space--l);
+  }
+`;
+
+IconButton.displayName = 'IconButton';
+
+IconButton.propTypes = {
+  className: PropTypes.string,
+  icon: PropTypes.oneOf(Icon.NAMES).isRequired,
+  size: PropTypes.oneOf(sizes),
+};
+
+IconButton.defaultProps = {
+  className: undefined,
+  size: Icon.SIZES.L,
+};
+
+IconButton.ICONS = Icon.ICONS;
+
+export default IconButton;
