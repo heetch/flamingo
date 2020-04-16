fa177c6fe0d92f91fa33056ff9a67be9be1b5af1
Obed <obed.imp@gmail.com>
2020-04-16 17:00:27 +0200

    Migrate Text

---
+++ src/components/Text/index.js
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+import styled from 'styled-components';
+
+import UiText from '../UiText';
+
+const variants = Object.values(UiText.VARIANTS);
+
+const Text = styled(UiText).attrs(({ className }) => ({
+  className: cx('f-Text', className),
+}))`
+  color: var(--f-color-text--secondary);
+  text-align: ${({ isNumber }) => isNumber && 'right'};
+`;
+
+Text.displayName = 'Text';
+
+Text.propTypes = {
+  as: PropTypes.string,
+  className: PropTypes.string,
+  isNumber: PropTypes.bool,
+  variant: PropTypes.oneOf(variants),
+};
+
+Text.defaultProps = {
+  as: 'p',
+  isNumber: false,
+  variant: UiText.VARIANTS.content,
+};
+
+export default Text;
