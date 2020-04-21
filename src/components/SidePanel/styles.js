b14337cdd1753ef61d118954c9cc466e30397e0a
ugogo <hello@onali-ugo.fr>
2020-04-21 12:51:22 +0200

    Migrate SidePanel

--- src/components/SidePanel/styles.js
+++ src/components/SidePanel/styles.js
@@ -6,7 +6,7 @@ import Button from '../Button';
 const slideIn = keyframes`
   0% {
     transform: translateX(100%);
-  } 
+  }
   100%{
     transform: translateX(0);
   }
@@ -15,14 +15,14 @@ const slideIn = keyframes`
 const slideOut = keyframes`
   0% {
     transform: translateX(0);
-  } 
+  }
   100%{
     transform: translateX(100%);
   }
 `;
 
 export const StyledSidePanel = styled('div')`
-  --content-hSpacing: var(--f-space--xxl);
+  --content-hSpacing: ${({ theme }) => theme.space.xxl};
 
   position: fixed;
   top: 0;
@@ -31,13 +31,14 @@ export const StyledSidePanel = styled('div')`
   width: 100%;
   max-width: ${({ maxWidth }) => maxWidth || '33.75rem;'} /* 540px */
   overflow: auto;
-  z-index: calc(var(--f-zIndex--Overlay) + 1);
-  background-color: var(--f-color-element--primary);
+  z-index: ${({ theme }) => `calc(${theme.zIndex.overlay} + 1)`};
+  background-color: ${({ theme }) => theme.color.element.primary};
   animation: ${({ isOpen, animateOnMount }) =>
     isOpen ? animateOnMount && slideIn : slideOut} 0.2s ease-out;
+  pointer-events: ${({ isOpen }) => (isOpen ? 'inherit' : 'none')};
 
   @media (max-width: 460px) {
-    --content-hSpacing: var(--f-space--xl);
+    --content-hSpacing: ${({ theme }) => theme.space.xl};
   }
 `;
 
@@ -48,7 +49,7 @@ StyledSidePanel.propTypes = {
 };
 
 export const Content = styled('div')`
-  padding: var(--f-space--xxl) var(--content-hSpacing);
+  padding: ${({ theme }) => `${theme.space.xxl} var(--content-hSpacing)`};
 `;
 
 export const Header = styled('div')`
@@ -58,8 +59,8 @@ export const Header = styled('div')`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: var(--f-space--m) var(--content-hSpacing);
-  border-bottom: 1px solid var(--f-color-element--inactive);
+  padding: ${({ theme }) => `${theme.space.m} var(--content-hSpacing)`};
+  border-bottom: ${({ theme }) => `1px solid ${theme.color.element.inactive}`};
 `;
 
 export const Footer = styled('div')`
@@ -67,10 +68,10 @@ export const Footer = styled('div')`
     bottom: 0;
     display: flex;
     justify-content: flex-end;
-    border-top: 1px solid var(--f-color-element--inactive);
+    border-top: ${({ theme }) => `1px solid ${theme.color.element.inactive}`};
     padding: 0 var(--content-hSpacing);
 
    ${Button} + ${Button} {
-    margin-left: var(--f-space--m);
+    margin-left: ${({ theme }) => theme.space.m};
   }
 `;
