f33c4a1dfe1e44f25799ac4e899ebca353798c29
ugogo <hello@onali-ugo.fr>
2020-04-22 09:28:31 +0200

    Upgrade deps

---
+++ .storybook/preview.js
@@ -0,0 +1,46 @@
+import { addParameters, addDecorator } from '@storybook/react';
+import { addons } from '@storybook/addons';
+import { withKnobs } from '@storybook/addon-knobs';
+import { withInfo } from '@storybook/addon-info';
+
+import theme from './theme/';
+import StoryWrapper from './theme/StoryWrapper';
+
+import './styles.css'
+import 'normalize.css';
+
+addons.setConfig({
+  theme,
+});
+
+addDecorator(StoryWrapper);
+addDecorator(withKnobs);
+addDecorator(withInfo);
+
+addParameters({
+  viewport: {
+    viewports: {
+      flamingo_s: {
+        name: 'Flamingo breakpoint S',
+        styles: {
+          width: '460px',
+          height: '963px',
+        },
+      },
+      BreakpointM: {
+        name: 'Flamingo breakpoint M',
+        styles: {
+          width: '800px',
+          height: '963px',
+        },
+      },
+      BreakpointL: {
+        name: 'Flamingo breakpoint L',
+        styles: {
+          width: '1100px',
+          height: '963px',
+        },
+      },
+    },
+  },
+});
