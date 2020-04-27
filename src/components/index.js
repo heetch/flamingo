ce11428e0eecdff6801e891dab68e4f25f530da3
Obed <obed.imp@gmail.com>
2020-04-27 18:10:06 +0200

    Add Alert back

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-// export { default as Alert } from './Alert';
+export { default as Alert } from './Alert';
 export { default as Button } from './Button';
 export { default as Card } from './Card';
 export { default as Checkbox } from './Checkbox';
