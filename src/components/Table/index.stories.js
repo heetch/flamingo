1c357d34d0b3298470a0e9406dc4f8510a845544
ugogo <hello@onali-ugo.fr>
2020-04-29 13:50:41 +0200

    Update Table stories

--- src/components/Table/index.stories.js
+++ src/components/Table/index.stories.js
@@ -8,59 +8,56 @@ import Icon from '../Icon';
 import Table from '.';
 import Text from '../Text';
 
-const data = [...new Array(7)].map(() => ({
-  string: Math.random(),
-  number: Math.random().toFixed(2),
-  custom: 'https://myrealdomain.com/images/avatar-icon-5.png',
-}));
-
 const stories = storiesOf('Table', module);
+
+const defaultProps = {
+  data: [...new Array(7)].map(() => ({
+    string: Math.random(),
+    number: Math.random().toFixed(2),
+    custom: 'https://myrealdomain.com/images/avatar-icon-5.png',
+  })),
+  columns: [
+    {
+      Header: text("Col #1's heading", 'With string'),
+      accessor: 'string',
+    },
+    {
+      Header: text("Col #2's heading", 'With number'),
+      accessor: 'number',
+      Cell: item => <Text isNumber>{item.cell.value}</Text>,
+    },
+    {
+      Header: text("Col #3's heading", 'With custom rendering'),
+      accessor: 'custom',
+      Cell: () => <Icon icon={Icon.ICONS.IconMoon} />,
+    },
+  ],
+};
+
+stories.add('All states', () => (
+  <>
+    <Heading>Table</Heading>
+    <Table {...defaultProps} />
+
+    <Heading level={2}>Sortable</Heading>
+    <Table {...defaultProps} isSortable />
+
+    <Heading level={2}>With manual sorting</Heading>
+    <Table
+      {...defaultProps}
+      manualSorting
+      initialState={{ sortBy: [{ id: 'number', desc: true }] }}
+    />
+  </>
+));
 
 stories.add('Playground', () => (
   <>
     <Heading>Table</Heading>
     <Table
-      columns={[
-        {
-          Header: text("Col #1's heading", 'With string'),
-          accessor: 'string',
-        },
-        {
-          Header: text("Col #2's heading", 'With number'),
-          accessor: 'number',
-          Cell: item => <Text isNumber>{item.cell.value}</Text>,
-        },
-        {
-          Header: text("Col #3's heading", 'With custom rendering'),
-          accessor: 'custom',
-          Cell: () => <Icon icon={Icon.ICONS.IconMoon} />,
-        },
-      ]}
-      data={data}
+      {...defaultProps}
       isSortable={boolean('Table 1 sortable', false)}
-    />
-
-    <Heading>Table with manual sorting</Heading>
-    <Table
-      columns={[
-        {
-          Header: text("Col #1's heading", 'With string'),
-          accessor: 'string',
-        },
-        {
-          Header: text("Col #2's heading", 'With number'),
-          accessor: 'number',
-          Cell: item => <Text isNumber>{item.cell.value}</Text>,
-        },
-        {
-          Header: text("Col #3's heading", 'With custom rendering'),
-          accessor: 'custom',
-          Cell: () => <Icon icon={Icon.ICONS.IconMoon} />,
-        },
-      ]}
-      data={data}
-      isSortable={boolean('Table 2 sortable', true)}
-      manualSorting
+      manualSorting={boolean('manualSorting')}
       onChangeSort={action('onChangeSort')}
       initialState={{ sortBy: [{ id: 'number', desc: true }] }}
     />
