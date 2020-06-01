50f1dccdb8bf8faa7752fb4c6af534f71edee7d4
Yohan Robert <yohan.robert@outlook.fr>
2020-06-01 12:47:57 +0200

    Address PR comments

--- src/components/Tabs/index.stories.js
+++ src/components/Tabs/index.stories.js
@@ -5,21 +5,30 @@ import { array } from '@storybook/addon-knobs';
 import Heading from '../Heading';
 import Tabs from '.';
 
+const TAB_VALUES = [
+  'Cars',
+  'Drivers',
+  'Rides',
+  'Vehicles',
+  'Discounts',
+  'Promotions',
+];
+
 const stories = storiesOf('Tabs', module);
+
+stories.add('All states', () => (
+  <>
+    <Heading>Tabs</Heading>
+    <Heading level={2}>Default</Heading>
+    <Tabs elements={TAB_VALUES} />
+    <Heading level={2}>With default index</Heading>
+    <Tabs defaultIndex={1} elements={TAB_VALUES} />
+  </>
+));
 
 stories.add('Playground', () => (
   <>
     <Heading>Tabs</Heading>
-    <Tabs
-      defaultIndex={1}
-      elements={array('Tabs (separated by comma)', [
-        'Cars',
-        'Drivers',
-        'Rides',
-        'Vehicles',
-        'Discounts',
-        'Promotions',
-      ])}
-    />
+    <Tabs elements={array('Tabs (separated by comma)', TAB_VALUES)} />
   </>
 ));
