83089a9c7513901600f20cfab11041f205ff4601
Octave Stoufflet <octave@heetch.com>
2021-04-14 17:16:15 +0200

    Badge and playground
    add variant
    
    add playground
    
    last fix
    
    Tam's comment

---
+++ src/components/Badge/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import { theme } from '../../theme';
+
+export const RoundedBadge = styled.div`
+  display: inline-block;
+  border-radius: ${({ borderRadius }) => borderRadius || theme.borderRadius.xl};
+  padding: 2px 10px;
+  color: ${({ color }) => color || theme.color.text.primary};
+  background-color: ${({ backgroundColor }) =>
+    backgroundColor || theme.color.element.primary};
+  font-size: ${({ fontSize }) => fontSize || theme.fontSize.s};
+  font-weight: ${({ fontWeight }) => fontWeight || theme.fontWeight.normal};
+`;
